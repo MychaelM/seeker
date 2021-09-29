8e0154bc90ce94e68a6bfca010c13fa98e70c3f0
Mychael Menges <mychael.menges@mx.com>
2021-09-28 21:17:42 -0600

    added click on sign in button inside my account dropdown homepage

--- src/sites/barnesNNoble/BarnesNNoble.ts
+++ src/sites/barnesNNoble/BarnesNNoble.ts
@@ -1,11 +1,19 @@
-import * as puppeteer from 'puppeteer';
-import { urls } from './references'
+import puppeteer from 'puppeteer';
+import { urls, selectors } from './references'
 
 async function start(): Promise<void> {
-  const browser = await puppeteer.launch({ headless: false });
+  const browser = await puppeteer.launch({ headless: false, defaultViewport: { height: 1080, width: 1920 }});
   const page = await browser.newPage();
-
   await page.goto(`${urls.homepage}`, { waitUntil: "domcontentloaded"});
+  await page.hover(selectors.myAccountDropdown)
+
+  const [signInBtn] = await page.$x(selectors.signInBtn);
+  await signInBtn.click()
+  await page.waitForSelector(selectors.emailUsername);
+  // await page.keyboard.type("email@email.com", { delay: 100 })
+  // await page.keyboard.type("email@email.com", { delay: 100 })
+
+  await page.screenshot({ path: `puppeteerDownloads/screenshot${new Date()}.png`})
 
   await browser.close()
 }
--- src/sites/barnesNNoble/references.ts
+++ src/sites/barnesNNoble/references.ts
@@ -1,6 +1,9 @@
 const selectors = {
-
-}
+  "myAccountDropdown": "#navbarDropdown",
+  "signInBtn": "/html/body/div[1]/header/nav/div/div[2]/ul[2]/li[1]/div/dd/a[1]",
+  "emailUsername": "#email",
+  "password": "#password"
+};
 
 const urls = {
   "homepage": "https://www.barnesandnoble.com"
